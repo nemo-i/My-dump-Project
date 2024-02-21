3323994cef0a5b0e06fe1220df28be6935ba24e3
nemo-i <tanton509@outlook.com>
2024-02-21 18:02:57 +0200

    list all files and send it over server

--- dump_ftp/index.js
+++ dump_ftp/index.js
@@ -1,10 +1,24 @@
 const path = require('path');
 const fs = require('fs');
+const express = require('express');
+const app = express();
+const PORT = 4060;
 
-console.log(__dirname);
 
-fs.readdir(__dirname, (err, files) => {
-    for (const file of files) {
-        console.log(file);
-    }
+const data = [];
+app.get('/', (req, res) => {
+    fs.readdir(__dirname, (err, files) => {
+        for (const file of files) {
+            data.push(file);
+        }
+    });
+    res.send(data);
 });
+
+
+
+app.listen(PORT, () => {
+    console.log("Start Listen On Port 4060");
+});
+
+
